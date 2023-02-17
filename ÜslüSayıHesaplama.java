e59c0de189756c67dd68c21a3a43a6c5bd8ffd5e
Hasan Hüseyin Gümüştepe <116903648+HSN51@users.noreply.github.com>
2023-02-17 19:52:03 +0300

    Java Üslü Sayı Hesaplama
    www.patikadev.com

---
+++ ÜslüSayıHesaplama.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ÜslüSayıHesaplama {
+    public static void main(String[] args) {
+
+        int n,k;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı : ");
+        n = inp.nextInt();
+
+        System.out.print("Üs olacak sayı : ");
+        k = inp.nextInt();
+
+        int total = 1;
+
+        for (int i = 1; i<=k ; i++){
+
+            total *= n;
+
+        }
+        System.out.println("Cevap : " + total);
+
+    }
+}
